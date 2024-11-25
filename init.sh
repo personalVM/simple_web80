daa142d7b8387c1d538aaf58637e48889e6fa51a
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-11-25 01:19:59 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-docker build -t simple-website -f /home/guilhermeviegas1993/simple_web80/Dockerfile .
-docker run -d -p 80:80 simple-website
+sudo docker build -t simple-website -f /home/guilhermeviegas1993/simple_web80/Dockerfile .
+sudo docker run -d -p 80:80 simple-website
